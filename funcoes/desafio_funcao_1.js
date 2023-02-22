5adf70f27385bd5acd7c9731c579162461784121
Anderson Bispo <andersonsbispo1997@gmail.com>
2023-02-22 20:24:53 -0300

    fim do modulo 6

---
+++ funcoes/desafio_funcao_1.js
@@ -0,0 +1,15 @@
+// elaborar um função que receberá 2 parâmetros -> console.log(+)
+function somar(n1, n2) {
+    console.log(n1 + n2);
+}
+
+somar(3, 4);
+somar(31, 98);
+
+// elaborar um função que receberá 2 parâmetros -> console.log(-)
+function subtrair(n1, n2) {
+    console.log(n1 - n2);
+}
+
+subtrair(11, 7);
+subtrair(19, 52);
